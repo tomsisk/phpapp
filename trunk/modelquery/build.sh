9509efedadb2be9280ba8240abcfdd1070e898c8
jjongsma@gmail.com <jjongsma@gmail.com@7d8eb290-d689-f5d8-4a1c-ade9f819b63c>
2011-01-11 23:17:05 +0000

    Fixed build path

--- trunk/modelquery/build.sh
+++ trunk/modelquery/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
-tar czvf modelquery/modelquery.tar.gz modelquery/modelquery/ modelquery/test/ modelquery/bin/ \
+tar czvf modelquery/modelquery.tar.gz modelquery/src/modelquery/ modelquery/test/ modelquery/bin/ \
 		modelquery/README modelquery/INSTALL modelquery/LICENSE modelquery/AUTHORS \
 		modelquery/CHANGELOG \
 		--exclude=**/.svn
