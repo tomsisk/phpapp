ad50f22826f47db0b47cb1d6975d5d9c285a938c
jjongsma <jjongsma@gmail.com>
2011-02-16 20:47:03 +0000

    Moved

--- modelquery/test/AllTests.class.php
+++ modelquery/test/AllTests.class.php
@@ -19,7 +19,7 @@
 	 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 	 */
 
-	require_once('phpunit.php');
+	require_once('lib/phpunit.php');
 
 	class AllTests extends TestCase {
 		public function suite() {
--- modelquery/test/test.php
+++ modelquery/test/test.php
@@ -19,8 +19,8 @@
 	 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 	 */
 
-	require_once('phpunit.php');
-	require_once('PlainTextTestResult.class.php');
+	require_once('lib/phpunit.php');
+	require_once('lib/PlainTextTestResult.class.php');
 	require_once('AllTests.class.php');
 
 	$suite = new TestSuite();
